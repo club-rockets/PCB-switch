bc213b9dbb4515fb11fc9e14e39902ada1258336
David Bourgault <dav.bour@gmail.com>
2018-06-08 09:57:56 -0400

    Moved init and button code to Relay struct

--- rocket-key-fob.ino
+++ rocket-key-fob.ino
@@ -7,52 +7,56 @@
 #define ssid "Secret Wifi Arming" // WiFi SSID
 #define password "password"       // WiFi password
 
-struct Relay {
-  String name;
-  int pin_on;
-  int pin_off;
-  bool status;
+struct Relay
+{
+    String name;
+    int pin_on;
+    int pin_off;
+    bool status;
 
-  Relay(String name, int on, int off) {
-      Relay::name = name;
-      pin_on = on;
-      pin_off = off;
+    Relay(String name, int on, int off)
+    {
+        Relay::name = name;
+        pin_on = on;
+        pin_off = off;
 
-      pinMode(pin_on, OUTPUT);
-      digitalWrite(pin_on, LOW);
-      pinMode(pin_off, OUTPUT);
-      digitalWrite(pin_off, HIGH);
+        pinMode(pin_on, OUTPUT);
+        digitalWrite(pin_on, LOW);
+        pinMode(pin_off, OUTPUT);
+        digitalWrite(pin_off, HIGH);
 
-      status = false;
-  }
+        status = false;
+    }
+
+    String button()
+    {
+        String html = "<button class=\"";
+        html += (status ? "armed" : "disarmed");
+        html += "\" type=\"submit\" name=\"" + name + "\">";
+        html += name + (status ? " - ARMED" : " - DISARMED");
+        html += "</button>";
+        return html;
+    }
 };
 
-Relay* relays[3] = { nullptr, nullptr, nullptr };
+Relay *relays[3] = {nullptr, nullptr, nullptr};
 String infolog;
 // Création des objets / create Objects
 ESP8266WebServer server(80);
 
-String makeButton(Relay* relay) {
-  String button = "<button class=\"";
-  button += (relay->status ? "armed" : "disarmed");
-  button += "\" type=\"submit\" name=\"" + relay->name + "\"";
-  button += " value=\"yes\">";
-  button += relay->name;
-  button += "</button>";
-  return button;
+String makeButton(Relay *relay)
+{
 }
 
 String getPage()
 {
     String page = "<html lang=fr-FR><head><meta http-equiv='refresh' content='10'/>";
-    page += "<title>RockÉTS</title>";
-    page += "<style> body { background: black; font-family: Source Code Pro, monospace; Color: white } button { width: 100%; height: 14%; font-size: 2em } button.disarmed { background: green } button.armed { background: red }</style>";
+    page += "<title>RockETS</title>";
+    page += "<style> body { background: black; font-family: Source Code Pro, monospace; Color: white } button { width: 100%; height: 20%; font-size: 3em; border:none; margin-bottom: 1% } button.disarmed { background: green } button.armed { background: red } div#log { height: 20%; overflow: scroll }</style>";
     page += "</head><body><h1>RockETS</h1>";
     page += "<form action='/' method='POST'>";
-    page += makeButton(relays[0]);
-    page += makeButton(relays[1]);
-    page += makeButton(relays[2]);
-    page += "</form><div style=\"overflow: scroll\">";
+    page += relays[0]->button() + relays[1]->button() + relays[2]->button();
+    page += "</form><div id=\"log\">";
     page += infolog;
     page += "</div></body></html>";
     return page;
@@ -60,18 +64,18 @@ String getPage()
 void handleRoot()
 {
     //Channel 1
-    for (int i = 0; i < 3; ++i) {
-        if (server.hasArg(relays[i]->name)) {
-              relays[i]->status = !relays[i]->status;
-              digitalWrite(relays[i]->pin_off, LOW);
-              digitalWrite(relays[i]->pin_off, LOW);
-              digitalWrite(relays[i]->pin_off, !relays[i]->status);
-              digitalWrite(relays[i]->pin_on, relays[i]->status);
-              infolog += "switched ";
-              infolog += relays[i]->name;
-              infolog += " at pos ";
-              infolog += String(i);
-              infolog += "<br/>";
+    for (int i = 0; i < 3; ++i)
+    {
+        if (server.hasArg(relays[i]->name))
+        {
+            relays[i]->status = !relays[i]->status;
+            digitalWrite(relays[i]->pin_off, LOW);
+            digitalWrite(relays[i]->pin_off, LOW);
+            digitalWrite(relays[i]->pin_off, !relays[i]->status);
+            digitalWrite(relays[i]->pin_on, relays[i]->status);
+
+            String line = "switched " + relays[i]->name + (relays[i]->status ? " ON" : " OFF") + "<br/>";
+            infolog = line + infolog;
         }
     }
     server.send(200, "text/html", getPage());
@@ -80,7 +84,6 @@ void handleRoot()
 void setup()
 {
     WiFi.softAP(ssid, password);
-    IPAddress myIP = WiFi.softAPIP();
 
     server.on("/", handleRoot);
     server.begin();
@@ -95,4 +98,3 @@ void loop()
     server.handleClient();
     delay(200);
 }
-
